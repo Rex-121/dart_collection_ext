e2787c2255ab2dd065afcf02297979dc77661eb8
Tyrant <>
2021-05-08 18:09:43 +0800

    add zipped

--- lib/src/collection_ext_base.dart
+++ lib/src/collection_ext_base.dart
@@ -1,13 +1,16 @@
-// TODO: Put public facing types in this file.
-
 import 'dart:math';
 
 extension Extension<T> on List<T> {
-
   void zip<Z>(Iterable<Z> other, Function(T, Z) zipper) {
     if (isEmpty || other.isEmpty) return;
     for (var i = 0; i < min(length, other.length); i++) {
       zipper(elementAt(i), other.elementAt(i));
     }
+  }
+
+  List<K> zipped<Z, K>(Iterable<Z> other, K Function(T, Z) zipper) {
+    var ks = <K>[];
+    zip(other, (t, z) => ks.add(zipper(t, z)));
+    return ks;
   }
 }
--- test/collection_ext_test.dart
+++ test/collection_ext_test.dart
@@ -3,12 +3,13 @@ import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    
     test('First Test', () {
-
-      [1,1,1,1,1].zip([3,4,5,], (a, b) => {
-        print(a+b)
-      });
+      [1, 1, 1, 1, 1].zip([
+        3,
+        4,
+        5,
+      ], (a, b) => {print(a + b)});
+      print([1, 1, 1, 1, 1].zipped([3, 4, 5], (a, b) => a + b));
     });
   });
 }
